4f692148f92bfcd27ac4a3288d3af933fbe917c9
maftah <sirleemaftah@gmail.com>
2023-03-12 08:43:28 +0300

    initial commit

---
+++ books/tests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Book
+
+class BookTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title = "A good Title",
+            subtitle = "An Excelent Subtitle",
+            author = "William Shakespear",
+            isbn = 4567342589071,
+        )
+    
+    def test_book_content(self):
+        self.assertEqual(self.book.title, "A good Title")
+        self.assertEqual(self.book.subtitle, "An Excelent Subtitle")
+        self.assertEqual(self.book.author, "William Shakespear")
+        self.assertEqual(self.book.isbn, 4567342589071)
+    
+    def test_book_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Excellent Subtitle")
+        self.assertTemplateUsed(response, "books/book_list.html")
+
+
+
