c9d7b0253e3b138c09bba149083bda060f8a48ea
maftah <sirleemaftah@gmail.com>
2023-03-23 13:58:26 +0300

    update debug status in settings

--- library/settings.py
+++ library/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c_k1)7e&(2@iwyhjyb+4d11cv$9p8zlf3v-56@=--9ho-#)07^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
